55786af0a9cd6763718eb5ac9c7986e5efd451b6
Ashik Panchkoti <Ashik-p@users.noreply.github.com>
2025-10-17 09:24:34 -0700

    Remove unused worker folder

--- src/react-app/worker/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-
-// If Cloudflare Workers `Env` type isn't available locally, define a minimal fallback
-type WorkerEnv = Record<string, unknown>;
-
-const app = new Hono<{ Bindings: WorkerEnv }>();
-
-export default app;
